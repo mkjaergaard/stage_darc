9ef3ab2042252a0b51e7eaa7e78724531dd1fab2
Morten Kjaergaard <mkjargaard@willowgarage.com>
2012-01-12 10:42:09 -0800

    initial version

---
+++ src/stage_node.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <boost/thread.hpp>
+#include <darc/node.h>
+#include <darc/registry.h>
+
+int main(int argc, const char* argv[])
+{
+  // Create Node
+  darc::Node::Ptr node = darc::Node::create();
+
+  // Create and run Component1
+  darc::Component::Ptr c1 = node->instantiateComponent( "StageComponent" );
+  boost::thread t1( boost::bind(&darc::Component::run, c1) );
+
+  // You can also manually construct a component and call the run() method if you want.
+  // But using the register allows for other cool stuff. E.g. starting remotely.
+
+  // Set up node connections
+  node->setNodeID(125);
+  node->accept("udp://127.0.0.1:5125");
+  node->connect(120, "udp://127.0.0.1:5120");
+
+  // Run Node in main thread
+  node->run();
+  return 0;
+}
+
